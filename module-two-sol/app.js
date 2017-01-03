69f23ecb2459bd7e810f4fe1564ca4a9a53ba936
wuhyn <hong.yang.ng@gmail.com>
2017-01-04 04:49:57 +1100

    Added service

--- module-two-sol/app.js
+++ module-two-sol/app.js
@@ -1,40 +1,59 @@
 (function (){
   'use strict';
 
-  var toBuyList = [
-    {
-      name: "Cookies",
-      quantity: "10"
-    },
-    {
-      name: "Bananas",
-      quantity: "5"
-    },
-    {
-      name: "Chicken wings",
-      quantity: "9"
-    },
-    {
-      name: "Donuts",
-      quantity: "12"
-    },
-    {
-      name: "Apples",
-      quantity: "6"
-    }
-  ];
-
   //Declare Angular Module
   angular.module('ShoppingListCheckOff', [])
 
   .controller('ToBuyController', ToBuyController)
   .controller('AlreadyBoughtController', AlreadyBoughtController)
+  //Create once service, use it to inject
   .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  ToBuyController.$inject = ['$scope', '$injector'];
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService){
+    var buy = this;
 
-  function ToBuyController($scope, $injector){
-    $scope.toBuyList = toBuyList;
+    buy.items = ShoppingListCheckOffService.getOriginalItems();
+  }
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService){
+
+  }
+
+  function ShoppingListCheckOffService(){
+    var service = this;
+
+    var originalItems = [
+      {
+        name: "Cookies",
+        quantity: "10"
+      },
+      {
+        name: "Bananas",
+        quantity: "5"
+      },
+      {
+        name: "Chicken wings",
+        quantity: "9"
+      },
+      {
+        name: "Donuts",
+        quantity: "12"
+      },
+      {
+        name: "Apples",
+        quantity: "6"
+      }
+    ];
+
+    //Used in the AlreadyBought Context
+    var boughtItems = [];
+
+    service.getOriginalItems = function(){
+      return originalItems;
+    };
+
   }
 
 
