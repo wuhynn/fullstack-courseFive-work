6d006ee51bf7482076cdafb0d9476b9515b0fcb7
wuhyn <hong.yang.ng@gmail.com>
2017-01-04 15:50:49 +1100

    Ng-if functionality added

--- module-two-sol/app.js
+++ module-two-sol/app.js
@@ -9,21 +9,35 @@
   //Create once service, use it to inject
   .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
+  //ToBuyController and inject service
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
   function ToBuyController(ShoppingListCheckOffService){
     var buy = this;
 
-    buy.items = ShoppingListCheckOffService.getOriginalItems();
+    //Use for displaying to buy list of items
+    buy.originalItems = ShoppingListCheckOffService.getOriginalItems();
+
+    //Use removeItem function
+    buy.removeItem = function(index){
+      ShoppingListCheckOffService.removeItem(index);
+
+    };
   }
 
+  //AlreadyBoughtController and inject the service
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
   function AlreadyBoughtController(ShoppingListCheckOffService){
+    var bought = this;
+
+    //Used to display bought items
+    bought.boughtItems = ShoppingListCheckOffService.getBoughtItems();
 
   }
 
   function ShoppingListCheckOffService(){
     var service = this;
 
+    //The initial items on the list
     var originalItems = [
       {
         name: "Cookies",
@@ -50,11 +64,26 @@
     //Used in the AlreadyBought Context
     var boughtItems = [];
 
+    //For displaying the items
     service.getOriginalItems = function(){
       return originalItems;
     };
 
+    //For displaying bought items
+    service.getBoughtItems = function(){
+      return boughtItems;
+    };
+
+    //Removing the items
+    service.removeItem = function(index){
+      //push onto the boughtItems array
+      boughtItems.push(originalItems[index]);
+
+      //remove
+      originalItems.splice(index, 1);
+    };
   }
 
 
 })();
+//IEFE
